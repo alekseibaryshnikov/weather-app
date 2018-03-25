4720870f506d78afc4d58a0cd0d560f862a39fc8
alekseibaryshnikov <a250188@yandex.ru>
2018-03-25 14:30:58 +0300

    Get geocode from consle

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            describe: 'The user address for fetching weather by address.',
+            alias: 'address',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=117535%203-%D0%B9%20%D0%94%D0%BE%D1%80%D0%BE%D0%B6%D0%BD%D1%8B%D0%B9%20%D0%BF%D1%80%D0%BE%D0%B5%D0%B7%D0%B4%20%D0%B45.%20%D0%BA.2%20%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&key=AIzaSyAkfxWhTdwHBhTKM0TE834dNe0ocPz4PBE',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.a)}&key=AIzaSyAkfxWhTdwHBhTKM0TE834dNe0ocPz4PBE`,
     json: true
 }, (err, res, body) => {
-    console.log(body);
+    if (err) {
+        console.error(JSON.stringify(err, undefined, 2));
+    }
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
